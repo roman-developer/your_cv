751d43fe9fedf404dd9055adf9cd633cc50aeec9
Jose Maria Roman <jose.maria.roman87@gmail.com>
2016-06-21 18:41:34 +0200

    optimized profile screen

--- app/assets/javascripts/components/profiles/profile.js.jsx
+++ app/assets/javascripts/components/profiles/profile.js.jsx
@@ -1,25 +1,15 @@
 var Profile = React.createClass({
-  propTypes: {
-    name: React.PropTypes.string,
-    lastName: React.PropTypes.string,
-    age: React.PropTypes.node,
-    phone: React.PropTypes.node,
-    url1: React.PropTypes.string,
-    url2: React.PropTypes.string,
-    url3: React.PropTypes.string
+  getInitialState: function() {
+    return { data: this.props.data };
   },
-
-  render: function() {
-    return (
+  render: function(){
+    return(
       <div>
-        <ul>
-        <li><b>Nombre:</b> {this.props.name}</li>
-        <li><b>Apellidos:</b>  {this.props.lastName}</li>
-        <li><b>Edad:</b>  {this.props.age}</li>
-        <li><b>Telefono:</b> {this.props.phone}</li>
-        <li><b>Url1:</b> {this.props.url1}</li>
-        <li><b>Url2:</b> {this.props.url2}</li>
-        <li><b>Url3:</b> {this.props.url3}</li>
+        <ul className="list-group">
+          <li className="list-group-item"><b>Nombre:</b> {this.state.data.name}</li>
+          <li className="list-group-item"><b>Apellidos:</b>  {this.state.data.last_name}</li>
+          <li className="list-group-item"><b>Edad:</b>  {this.state.data.age}</li>
+          <li className="list-group-item"><b>Telefono:</b> {this.state.data.phone}</li>
         </ul>
       </div>
     );
