4c098e5179046474537c9f1fc5796c9bfde29976
Jose Maria Roman <jose.maria.roman87@gmail.com>
2016-06-29 19:53:17 +0200

    generate pdf with styles and fix some bugs

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -10,5 +10,6 @@ class Curriculum < ActiveRecord::Base
 	accepts_nested_attributes_for :educations, reject_if: :all_blank, allow_destroy: true
 	accepts_nested_attributes_for :courses, reject_if: :all_blank, allow_destroy: true
 
+	validates :mother_tongue, :professional_profile, presence: true
 
 end
