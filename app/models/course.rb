435560a564cb0b16f614856e1f18c39141c87b7b
Jose Maria Roman <jose.maria.roman87@gmail.com>
2016-07-11 19:29:59 +0200

    hot fix add validations to forms and add translations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,6 @@ class Course < ActiveRecord::Base
 
 	belongs_to :curriculum
 
+	validates :hours, numericality: { only_integer: true }
 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,8 @@ class Profile < ActiveRecord::Base
 
 	mount_uploader :avatar, AvatarUploader
 
+	validates :name, :last_name, :age, :phone, presence: true
+
 	def full_name
 		self.name + " " + self.last_name if self.name.present? && self.last_name.present?
 	end
